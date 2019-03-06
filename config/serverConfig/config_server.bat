d62e23145e7a1596fa1c3503a84d6b6e56acf837
snake <18501604724@163.com>
2019-03-06 23:54:08 +0800

    sss

--- config/serverConfig/config_server.bat
+++ config/serverConfig/config_server.bat
@@ -1,2 +1,2 @@
-mysql --local-infile=1 -uroot -h 47.100.113.40 -P 30000 -pprisma -DGame < ./config/serverConfig/config_servers.sql
-pause
+mysql --local-infile=1 -uroot -h 47.100.113.40 -P 30000 -pprisma -DGame < ./config_servers.sql
+pause 
